b0c417ef6b5bc89be56791c69850c224a1b68c10
vilra830 <villafuerte.rhea@gmail.com>
2024-12-10 15:15:40 +1100

    adds more style

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import { getRandomWord } from "./modules/math.js";
 import { createEl, removeEl } from "./modules/dom.js";
 const userInputSection = document.getElementById("user-input-section");
+const wordLength = document.getElementById("word-length");
+
 const newGameButton = document.getElementById("new-game-button");
 const newGameContainer = document.getElementById("new-game");
 const keyboard = document.getElementById("keyboard");
@@ -40,23 +42,36 @@ const keyboardKeys = [
   "M",
 ];
 
+/**This function resets the screen, empties the divs like keyboard, userInputSection,
+ * intializes wrongLetters and correct Letters back to zero
+ * shows full image of hangman
+ * New Game button is displayed to prompt user to play again
+ */
+
 const reset = () => {
   newGameButton.style.display = "block";
   userInputSection.innerText = "";
+  wordLength.innerHTML = "";
   keyboard.innerHTML = "";
-  hangmanImage.src = "././img/h-10.jpg";
   correctLetters = 0;
   wrongLetters = 0;
 };
 
-//when new game is clicked
+/**
+ * When new game button is clicked, it calls the startGame function.
+ * start games calls the getRandomWord function to generate a word , createEl function to generate the letter buttons inside the keyboard DIV, wrongCountImage function to
+ * generate images corresponding to the number of wrongLetters clicked.
+ *
+ */
 const startGame = () => {
   resultText.innerHTML = "";
   newGameButton.style.display = "none";
   let randWords = getRandomWord().toUpperCase();
   let displayItem = randWords.replace(/./g, '<span class="dashes">_</span>');
-  userInputSection.innerHTML = `<p> The word has ${displayItem.length} letters <p>`;
+  wordLength.innerHTML = `<p> The word has ${randWords.length} letters <p>`;
   userInputSection.innerHTML = displayItem;
+  console.log(randWords);
+  console.log(randWords.length);
   hangmanImage.src = "././img/h-4.jpg";
 
   for (let i = 0; i < keyboardKeys.length; i++) {
@@ -72,9 +87,11 @@ const startGame = () => {
             dashes[index].innerHTML = char;
             correctLetters += 1;
             if (correctLetters === randWord.length) {
-              resultText.innerHTML = `<h2>You guessed it right!!!</h2 class="winner"><p class="winner">The word was <span>${randWord.join(
+              hangmanImage.src = "././img/h-11.jpg";
+              resultText.innerHTML = `<h2>You guessed it right!!!</h2 class="winner"</h2><p class="winner">The word was <span>${randWord.join(
                 ""
               )}</span>.</p>`;
+
               reset();
             }
           }
@@ -95,7 +112,7 @@ const startGame = () => {
   }
 };
 
-//image that correponds to the number of wrong letters
+//A function that throws new image that correponds to the number of wrong letters
 const wrongCountimage = (wrongLetters) => {
   switch (wrongLetters) {
     case 1:
@@ -121,5 +138,5 @@ const wrongCountimage = (wrongLetters) => {
   }
 };
 
-//newgame
+// New Game button is clicked to start the game - calls the startGame function
 newGameButton.addEventListener("click", startGame);
